6a4cf1882eb8783fd26c55c5330280dc12e802f1
175552 <35876936+175552@users.noreply.github.com>
2020-06-23 10:22:17 -0500

    Add files via upload

---
+++ sensortest.py
@@ -0,0 +1,44 @@
+from sensordroid import Client
+
+import time
+
+def devicesDiscoveredEventHandler(devices):
+    print(devices)
+    if len(devices) > 0:
+        client = Client(devices[0])
+        client.connectionUpdated = connectionUpdatedEventHandler
+        client.sensorsReceived = sensorsReceivedEventHandler
+        client.imageReceived = cameraReceivedEventHandler
+        client.sensorsSampleRate = 50
+        client.cameraResolution = 13
+
+        client.connect()
+
+def connectionUpdatedEventHandler(sender, msg):
+    if sender is not None:
+        if sender.connected:
+            print("Connected")
+        else:
+            print("Disonnected")
+
+
+def sensorsReceivedEventHandler(sender, dataCurrent):
+    result = dataCurrent.Acceleration.Values.AsString
+    x = result.split(",")
+    currenttime = time.time() * (10.0)**9 - start_time
+    first = x[0].strip()
+    second = x[1].strip()
+    third = x[2].strip()
+    something = str(26) + "," + str(currenttime) + "," + first + "," + second + "," + third + ",waving" + "\n"
+    with open('user26.txt', 'a+') as f:
+        f.write(something)
+
+def cameraReceivedEventHandler(sender, image):
+    # Process image data bytes
+    pass
+
+start_time = time.time() * (10.0)**9;
+Client.devicesDiscovered = devicesDiscoveredEventHandler
+Client.startDiscovery()
+key = input("Press ENTER to exit\n")
+Client.closeAll()
